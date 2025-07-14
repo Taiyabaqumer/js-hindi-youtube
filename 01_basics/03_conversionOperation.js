f64cf1c7368a3c2f9604ee0f635cb03dc17a5dad
Taiyabaqumer <qumersana20@gmail.com>
2025-07-14 19:23:15 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,68 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//notes//
+// "33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1 // 1 => true, 0 => false,  "Sana"=> true , " " => false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+let someNumber = 33 // 33,string
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//****************Operations ********************
+
+let value =3 //-3
+let negValue = -value
+console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/3);
+//console.log(2**3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = "Sana"
+
+let str3 = str1 + str2
+console.log(str3);
+
+// console.log("1" + 2); 12
+// console.log(1 + "2"); 12
+// console.log("1" + 2 +2); 122
+// console.log(1+2 + "2"); 32
+
+console.log(+true); //1
+console.log(+""); //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter); // 101
+
+
+
+
+
+
+
